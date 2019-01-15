1bbc428ec8823653b859dec818c16f5000825f5f
Rogelio Cifuentes <nosoyiop@gmail.com>
2019-01-14 22:20:09 -0300

    Se ingresa metodo imprimir, desde base de datos

--- MiAplicacionAngular.js
+++ MiAplicacionAngular.js
@@ -1,29 +1,34 @@
 var app = angular.module('AppLogin', [])
 
-app.controller('ctrlLogin', function($scope)
-{
+app.controller('ctrlLogin', function($scope,$http){
     $scope.mostrarError = false;
     $scope.mostrarLogin = true;
+    $scope.comentarios = [];
 
-$scope.user = {
-    nombre: $scope.rut,
-    password: $scope.pass,
-};  //Creando un objeto
-
-$scope.validarLogin = function(){
-    if($scope.rut == "19.090.388-5" & $scope.pass == "12345"){
-        $scope.mostrarError = false;
-        $scope.mostrarLogin = false;
-    }else{
-        $scope.mostrarError = true;
-        $scope.mostrarLogin = true;
-
-    }
-
-};
-
+    $http.get("http://localhost:8080/comentarios/")
+        .then(function(data){
+            console.log(data.data);
+            $scope.comentarios = data.data;
         
+    },function(err){
+            });
 
+
+
+
+    $scope.user = {                               //Creando un objeto
+        nombre: $scope.rut,
+        password: $scope.pass,
+    };  
+
+    $scope.validarLogin = function(){
+        if($scope.rut == "19.090.388-5" & $scope.pass == "12345"){
+            $scope.mostrarError = false;
+            $scope.mostrarLogin = false;
+        }else{
+            $scope.mostrarError = true;
+            $scope.mostrarLogin = true;
+        }};
 
 });
 
